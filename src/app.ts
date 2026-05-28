f3c157d1a077e7f7bcaa85679a357197b577be2e
alwisusilo <alwisusilo@gmail.com>
2026-05-28 16:14:45 +0700

    feat: add create product with image

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { FRONTEND_URL, PORT } from "./config/config.js";
 import helmet from "helmet";
 import { AppError } from "./class/appError.js";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import productRoutes from "./modules/product.routes.js";
 
 const app = express();
 
@@ -45,6 +46,8 @@ app.use("/", (req: Request, _res: Response, next: NextFunction) => {
 app.get("/", (_req: Request, res: Response) => {
   res.send(`Auth boilerplate is running on port: ${PORT}`);
 });
+
+app.use("/products", productRoutes);
 
 //route not found handler
 app.use((_req, _res, next) => {
