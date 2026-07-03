6afb252dfced6febf8e21397187f4681607502e1
alizikrullah <ali.zikrullah@gmail.com>
2026-07-03 23:35:44 +0700

    fix : cors

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,8 @@ app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(helmet());
